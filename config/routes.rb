64ad064b6aa3a7e8a6a31cb9dc59cec9eb20633b
Tusnelda91 <julin@gmx.net>
2017-12-07 14:29:12 +0100

    added route to posts

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'pages/contact'
 
+  get 'pages/posts'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :posts
 
