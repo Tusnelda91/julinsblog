48eed69b65af072212efd36e0be09af45b1e0edd
Tusnelda91 <julin@gmx.net>
2017-12-08 13:27:26 +0100

    changed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get 'posts/index'
 
-  root 'posts#landing_page'
+  root 'posts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 resources :posts
